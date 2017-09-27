8129e4514282fa64241440d10a25179e8ad3a849
Tomek Bujakowski <bujakowski@gmail.com>
2017-09-27 13:32:57 +0200

    Implement base Stack

--- src/main/java/StackExample.java
+++ src/main/java/StackExample.java
@@ -1,18 +1,54 @@
+import java.util.Arrays;
+
 public class StackExample<T> {
 
-    private T [] data;
+    private T[] data;
+    private int stackPointer;
 
     public StackExample() {
-        this.data = (T []) new Object[1000];
+        this.data = (T[]) new Object[1000];
+        this.stackPointer = 0;
     }
 
+    public StackExample(int stackSize) {
+        this.data = (T[]) new Object[stackSize];
+        this.stackPointer = 0;
+    }
 
     public void push(T item) {
-
+        this.data[this.stackPointer++] = item;
     }
 
     public T pop() {
-        return null;
+        if (this.stackPointer == 0) throw new IndexOutOfBoundsException();
+        return this.data[--this.stackPointer];
     }
 
+    public boolean contains(T item) {
+        for (int i = 0; i < this.stackPointer; i++) {
+            if (this.data[i].equals(item)) return true;
+        }
+        return false;
+    }
+
+
+    public T access(T item) {
+        while (this.stackPointer > 0) {
+            T tempItem = this.pop();
+            if (item.equals(tempItem)) return tempItem;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public int size() {
+        return this.stackPointer;
+    }
+
+    @Override
+    public String toString() {
+        return "StackExample{" +
+                "data=" + Arrays.toString(data) +
+                ", stackPointer=" + stackPointer +
+                '}';
+    }
 }
